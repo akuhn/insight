95c347144639366eb2049b25f4b73dbed80f24bc
Adrian Kuhn <akuhn@iam.unibe.ch>
2013-01-31 22:48:45 -0800

    Showing time at sight on map.

--- app/app.py
+++ app/app.py
@@ -9,12 +9,20 @@ app = Flask(__name__)
 def hello_world():
     return render_template('map.html')
 
-@app.route('/vancouver/paths.json')
-def serve_json():
+@app.route('/vancouver/paths')
+def vancouver_paths():
     mongo = MongoClient()
     paths = mongo['4h'].paths.find()
     data = [[[each['latitude'],each['longitude']] for each in path['path']] for path in paths]
     return jsonify({'city':'vancouver','paths':data})
+
+@app.route('/vancouver/sights')
+def vancouver_sights():
+    mongo = MongoClient()
+    sights = mongo['4h'].poi.find(fields={'_id':False})
+    data = [each for each in sights]
+    return jsonify({'city':'vancouver','sights':data})
+
 
 if __name__ == '__main__':
     app.debug=True
